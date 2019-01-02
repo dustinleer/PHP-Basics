1e8f6aa00d64d05d0edb281260520bbb387dac80
Dustin Leer <dustin@wildcanon.com>
2019-01-02 16:37:40 -0500

    Learning about floats

--- numbers.php
+++ numbers.php
@@ -8,9 +8,15 @@
 	// echo "1";
 
 	// How do we tell if something is an integer? With the 'var_dump()' function
-	var_dump($num_one);
-	var_dump(1);
-	var_dump("1");
-	var_dump($num_one + $num_two - $num_three);
+	// var_dump($num_one);
+	// var_dump(1);
+	// var_dump("1");
+	// var_dump($num_one + $num_two - $num_three);
+
+	// Working with Floats
+	$distance_home = 1.2;
+	$distance_work = 2.5;
+
+	var_dump($distance_home + $distance_work);
 
 ?>
