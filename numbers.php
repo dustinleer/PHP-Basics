60ea30e4cc4291b0a8a02255c9cb34246e6efe30
Dustin Leer <dustin@wildcanon.com>
2019-01-02 16:26:49 -0500

    Updates readme.md and adds exercise files

---
+++ numbers.php
@@ -0,0 +1,16 @@
+<?php
+	$num_one = 1;
+	$num_two = 2;
+	$num_three = 3;
+
+	// echo $num_one;
+	// echo 1;
+	// echo "1";
+
+	// How do we tell if something is an integer? With the 'var_dump()' function
+	var_dump($num_one);
+	var_dump(1);
+	var_dump("1");
+	var_dump($num_one + $num_two - $num_three);
+
+?>
