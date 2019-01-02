ce36cdb99b505c16645199ad6865dbe96e323e35
Dustin Leer <dustin@wildcanon.com>
2019-01-02 16:54:50 -0500

    Working with operators

--- numbers.php
+++ numbers.php
@@ -3,6 +3,12 @@
 	$num_two = 2;
 	$num_three = 3;
 
+	/* Integers
+	 * 
+	 * This section went over variables and integers 
+	 * 
+	*/
+
 	// echo $num_one;
 	// echo 1;
 	// echo "1";
@@ -13,10 +19,47 @@
 	// var_dump("1");
 	// var_dump($num_one + $num_two - $num_three);
 
-	// Working with Floats
+	/* Floats
+	 * 
+	 * Working with Floats
+	 * 
+	*/
 	$distance_home = 1.2;
 	$distance_work = 2.5;
 
 	var_dump($distance_home + $distance_work + $num_three + .3);
+
+	/* Operators
+	 * 
+	 * This section covers other operators in PHP
+	 * 
+	*/
+	$a = 5;
+	$b = 10;
+
+	// Multiply
+	var_dump($a * $b);
+
+	// Quotient
+	var_dump($a / $b);
+
+	// Incrementing and Decrementing Operators
+	$a = $a + 1;
+	var_dump($a);
+	$a++;
+	var_dump($a);
+	$a--;
+	var_dump($a);
+	var_dump($a--);
+	var_dump($a);
+	var_dump(--$a);
+	var_dump($a);
+
+	// Combined Operators
+	$a = $a + 5;
+	var_dump($a);
+
+	$a += 5;
+	var_dump($a);
 
 ?>
